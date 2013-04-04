303d7bb5200c5bf8bf32a8c7ba180ca129bf12ff
dcollazo <davidcollazo@gmail.com>
2013-04-03 17:51:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmoothieApp::Application.config.secret_key_base = 'd0b2262ca0c479832a100ad785e10901506b0607460611405b0e645700294885054ebe16ba5de962b80cd5c91e1ec5ff6e8c091fd0db242d8c6bfa25bf8aac07'
